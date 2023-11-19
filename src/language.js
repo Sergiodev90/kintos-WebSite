6ca0d6bd16d81ef23139673a99e5b37d64e46ef5
sergio noriega cantillo <sergionoriegacantillo@gmail.com>
2023-11-19 21:22:58 +0100

    .

--- src/language.js
+++ src/language.js
@@ -1,28 +1,27 @@
 const arrow_down = document.querySelector(".icon-arrow");
-const language_image = document.querySelector(".language-tab")
 const languages = document.querySelector(".other-languages");
-arrow_down.addEventListener("click",addMenuLanguage);
-language_image.addEventListener("click",addMenuLanguage)
 
-function addMenuLanguage(){
-    languages.classList.toggle("inactive")
-    frames()
+arrow_down.addEventListener("click", addMenuLanguage);
+// Assuming you want to close the menu when any language tab is clicked
+languages.addEventListener("click", addMenuLanguage);
+
+function addMenuLanguage() {
+    languages.classList.toggle("inactive");
 }
 
-
-function frames(){
-    const animationClosed = languages.animate([
-        //keyframes
-        // {transform: "TranslateY(-50px)"},
-        // {transform: "TranslateY(0px)"},
-        {transform:"TranslateY(-50px)"},
-        {transform: "TranslateY(0)"}
-    ],{
-        //options
-        easing:"linear",
-        iterations:1,
-        duration:200 //milisegundos
-    })
-    return animationClosed.finished;
-}
+// function frames(){
+//     const animationClosed = languages.animate([
+//         //keyframes
+//         // {transform: "TranslateY(-50px)"},
+//         // {transform: "TranslateY(0px)"},
+//         {transform:"TranslateY(-50px)"},
+//         {transform: "TranslateY(0)"}
+//     ],{
+//         //options
+//         easing:"linear",
+//         iterations:1,
+//         duration:200 //milisegundos
+//     })
+//     return animationClosed.finished;
+// }
 
--- src/styles.js
+++ src/styles.js
@@ -1,16 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const header = document.querySelector('.Browser-container');
-    const body = document.querySelector('body');
-  
-    window.addEventListener('scroll', function () {
-      if (window.scrollY > 50) {
-        header.classList.add('shrink');
-      } else {
-        header.classList.remove('shrink');
-      }
-    });
+  const header = document.querySelector('.Browser-container');
+  const body = document.querySelector('body');
 
-    // Reset scroll position to top on page load
-    body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+  function updateHeaderClass() {
+    if (window.scrollY > 50) {
+      header.classList.add('shrink');
+    } else {
+      header.classList.remove('shrink');
+    }
+  }
+
+  window.addEventListener('scroll', function () {
+    requestAnimationFrame(updateHeaderClass);
+  });
+
+  // Reset scroll position to top on page load
+  body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 });
