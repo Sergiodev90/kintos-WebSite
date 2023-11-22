1905e09a4a39a3986f545f94f618c3412efec143
sergio noriega cantillo <sergionoriegacantillo@gmail.com>
2023-11-22 23:16:10 +0100

    i finished the responsive design and also i added a some designs

---
+++ src/styles.js
@@ -0,0 +1,28 @@
+alert("esta es la nueva pagina")
+const aside_mobile = document.querySelector(".background-menu-mobile")
+const aside_mobile_second = document.querySelector(".mobile-menu")
+const icon_menu = document.querySelector(".icon-menu")
+const icon_menu_aside = document.querySelector(".icon-menu-mobile")
+const background_mobile_gray = document.querySelector(".background-menu-mobile-Father")
+
+
+icon_menu.addEventListener("click",ShowAside)
+icon_menu_aside.addEventListener("click",toggleAside)
+function ShowAside(){
+  aside_mobile.classList.toggle("inactive")
+  aside_mobile_second.classList.toggle("inactive")
+  background_mobile_gray.classList.toggle("inactive")
+}
+
+function toggleAside(){
+  AsideClosedContainer = aside_mobile.classList.contains("inactive") 
+  AsideClosedSon= aside_mobile_second.classList.contains("inactive")
+
+  if(!AsideClosedContainer && !AsideClosedSon){
+    aside_mobile.classList.add("inactive") 
+    aside_mobile_second.classList.add("inactive")
+    background_mobile_gray.classList.add("inactive")
+  }
+
+}
+
